5d0f9b857aa797473f19c9b29de4f6099b0d15f2
shikhar <shikharskj21@gmail.com>
2021-01-09 19:51:51 +0530

    installed view, set up the view and view engine and created home.ejs(our first view/template for home page)

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 // module.exports.action_name = function(req,res){    };
 
 module.exports.home = function(req,res){
-    return res.end("<h1>HOME page : Express is up for Codeial!!</h1>");
+    return res.render("home",{
+        title:"HOME!!"
+    });
 };
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const port = 8000;
 const app = express();
 
+// set up the view engine
+app.set("view engine","ejs");
+app.set("views","./views");
 //use express router
 app.use("/",require("./routes"));
 
