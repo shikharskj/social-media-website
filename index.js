5d73c6e3fe7159893e4b022168063922f0f27ec2
shikhar <shikharskj21@gmail.com>
2021-01-13 17:28:45 +0530

    installed express-ejs-layouts and created a layout

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const port = 8000;
 const app = express();
+//require and set up express layout
+const expressLayouts = require("express-ejs-layouts");
+app.use(expressLayouts);
 
 // set up the view engine
 app.set("view engine","ejs");
 app.set("views","./views");
+
 //use express router
 app.use("/",require("./routes"));
 
