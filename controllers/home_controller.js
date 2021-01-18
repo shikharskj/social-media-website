edf9e007c431aa427e8ba595737a9917dea367aa
shikhar <shikharskj21@gmail.com>
2021-01-19 00:02:41 +0530

    Displayed posts and related users + checked authentication on creating posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,17 @@
 // module.exports.action_name = function(req,res){    };
-
+const Post = require("../models/post");
 module.exports.home = function(req,res){
     // console.log(req.cookies);
     // res.cookie("user_id",25);
-    return res.render("home",{
-        title:"HOME!!"
-    });
+
+    //populate the user of each post
+    Post.find({}).populate("user").exec(function(error,posts){
+        return res.render("home",{
+            title:"HOME!!",
+            posts: posts
+        });
+    })
+    
+
+    
 };
