9c5f2714bf8ffc0aeacbf9dca7e6075fd66082cf
shikhar <shikharskj21@gmail.com>
2021-03-24 17:24:32 +0530

    add and setup mailer for email notifications

---
+++ mailers/comments_mailer.js
@@ -0,0 +1,22 @@
+const nodeMailer = require('../config/nodemailer');
+
+// this is another way of exporting a method
+exports.newComment = (comment) => {
+    //console.log('inside newComment mailer', comment);
+    let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
+
+    nodeMailer.transporter.sendMail({
+       from: 'shikharskj21@gmail.com',
+       to: 'shikhar.nitsri@gmail.com',
+       subject: "New Comment Published!",
+       html: htmlString
+    }, (err, info) => {
+        if (err){
+            console.log('Error in sending mail', err);
+            return;
+        }
+
+        console.log('Message sent', info);
+        return;
+    });
+}
